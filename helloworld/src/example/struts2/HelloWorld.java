f877586d3a809fda517a5669b12672c5e7354e18
lewannanhai <33798932+lewannanhai@users.noreply.github.com>
2018-03-10 14:52:43 +0800

    helloworld
    这是strtus2的第一个例子

---
+++ helloworld/src/example/struts2/HelloWorld.java
@@ -0,0 +1,36 @@
+package example.struts2;
+
+public class HelloWorld {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	public String execute(){
+		
+		
+		if (getMessage().isEmpty()) {
+			
+			return "error";
+		}
+		
+		else{
+		
+		
+		return "success";
+		
+		}
+		
+		
+	}
+	
+	
+	
+}
